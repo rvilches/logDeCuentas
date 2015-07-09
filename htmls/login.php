6b5432f0357be678e0e7ffa85372e4a2154fe2e2
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-07-09 15:13:13 -0400

    pushing aloja

---
+++ htmls/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Cuentas</title>
+  <link rel="stylesheet" type="text/css" href="../css/login.css">
+</head>
+
+<body>
+<?php
+echo "Hellolala";
+?>
+ <h1>Estado de cuentas - DEV<span id="signup"><a href="htmls/signup.html"><i>Sign Up</i></a></span></h1>
+ 
+ <section>
+ 	<h3>Login:</h3>
+ 	<article>
+
+ 		<form>
+ 		<span class="formusername">Username:</span>
+ 		<br><input id="username" type="text" name="username" placeholder="username" ><br>
+ 		<span class="formpassword">Password:</span>
+ 		<br><input id="passwordinput" type="text" name="password" placeholder="password" ><br><br>
+ 		<a id="submit" value="login" href="htmls/home.html">Login</a>
+ 		<!-- <input type="submit" name="submit" value="Login" onclick="<a href=htmls/signup.html\">"><br> -->
+ 		
+		</form>
+
+ 				
+
+ 	</article>
+ </section>
+ <script type="text/javascript" src="login.js"></script>
+</body>
+</html>
