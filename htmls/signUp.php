cbbbd20bc1cf1022eab4ef1fec94f0855407c270
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-07-10 11:39:13 -0400

    add signup.php

--- htmls/signUp.php
+++ htmls/signUp.php
@@ -1,9 +1,10 @@
 <html>
 <body>
-
+<h1>HELOO??</h1>
  <?php 
  echo $_POST["firstname"]; <br>
-Your email address is:  echo $_POST["email"]; ?>
+Your email address is:  echo $_POST["email"]; 
+?>
 
 </body>
 </html> 
