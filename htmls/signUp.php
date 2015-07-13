8970e84398198fc82ea724ea8389690ff241fb61
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-07-13 13:07:05 -0400

    updating signUp.html

--- htmls/signUp.php
+++ htmls/signUp.php
@@ -4,34 +4,7 @@
 	<title>Subscribe</title>
 	<link rel="stylesheet" type="text/css" href="../css/signup.css">
 </head>
-<body onload = "getUser()">
-
-	<h2 id="bodyh2">Sign Up</h2>
-	<section>
-		<form id = "userForm" action=" " method="post">
-		
-		First Name:
-		<br><input type="text" name="firstname" placeholder="firstname" autofocus ="autofocus" onchange="getUser()" required><br><hr>
-		Lastname:
-		<br><input type="text" name="lastname" placeholder="lastname" onchange="getUser()" required ><br><hr>
-		Second Lastname:
-		<br><input type="text" name="secondLastname" placeholder="secondlastname" onchange="getUser()" required><br><hr>
-		Username:
-		<br><input type="text" name="username" placeholder="username" onchange="getUser()" required><br><hr>
-		Password:
-		<br><input type="password" name="password" placeholder="password" onchange="getUser()" required><br><hr>
-		
-		email:
-		<br><input type="text" name="email" placeholder="email" onchange="getUser()" required><span id="incEmail">* Invalid Email</span><br><hr>
-
-		<input id="submit" type="submit" name="submit" value="create account">
-
-
-		</form>
-
-
-	</section>
-<script type="text/javascript" src = "signup.js"></script>
+<body>
 
 <?php
 
