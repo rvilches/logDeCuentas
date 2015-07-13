d3aa3851548d899a452efe1d1e4f37a12a286c97
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-07-13 13:10:08 -0400

    updating signUp.html

--- htmls/signUp.php
+++ htmls/signUp.php
@@ -1,14 +1,10 @@
 <!DOCTYPE html>	
 <html lang="en">
-<head>
-	<title>Subscribe</title>
-	<link rel="stylesheet" type="text/css" href="../css/signup.css">
-</head>
 <body>
 
 <?php
 
- echo $_POST["firstname"]; 
+ <h1>echo $_POST["firstname"];</h1> 
  echo $_POST["lastname"]; 
 
 ?>
