1871dc695f08f1ce3f696d6d09bdad66fe2a61d4
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-07-13 16:18:28 -0400

    updating signUp.php

--- htmls/signUp.php
+++ htmls/signUp.php
@@ -33,7 +33,7 @@
  			} 
  			else 
  			{
- 				$email = test_input($POST["email"]);
+ 				$email = test_input($_POST["email"]);
  				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
  				{
   				$emailErr = "Invalid email format"; 
