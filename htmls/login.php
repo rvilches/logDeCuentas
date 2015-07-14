2ebd26d5cba186fc7a4e93796eeba1a2ab85597d
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-07-14 11:59:00 -0400

    update loginIndex.php

--- htmls/login.php
+++ htmls/login.php
@@ -33,7 +33,15 @@ try{
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     $sql ="SELECT * FROM users WHERE username='$username' AND password='$password'";
   	$conn->exec($sql);
-  	echo "$sql";
+  	$result = $conn->query($sql);
+  	if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
     
 	}
 catch(Exception $e)
