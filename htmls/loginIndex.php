9f863f933cab55cd53dc87cf510a0ae55c6fe99b
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-07-14 11:31:24 -0400

    change login.html for loginIndex.php

---
+++ htmls/loginIndex.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Cuentas</title>
+  <link rel="stylesheet" type="text/css" href="../css/login.css">
+</head>
+
+<body>
+<?php
+include 'login.php';
+?>
+ <h1>Estado de cuentas - DEV<span id="signup"><a href="htmls/signUp.php"><i>Sign Up</i></a></span></h1>
+ 
+ <section>
+ 	<h3>Login:</h3>
+ 	<article>
+
+ 		<form action="" method="post">
+ 		<span class="formusername">Username:</span>
+ 		<br><input id="username" type="text" name="username" placeholder="username" ><br>
+ 		<span class="formpassword">Password:</span>
+ 		<br><input id="passwordinput" type="text" name="password" placeholder="password" ><br><br>
+ 		<input type="submit" name="submit" value="Login"><br> 
+ 		
+		</form>
+
+ 				
+
+ 	</article>
+ </section>
+ <script type="text/javascript" src="login.js"></script>
+</body>
+</html>
