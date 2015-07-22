0cbc24944543aa388590030be47f531da980150c
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-07-22 16:39:49 -0400

    predo

--- htmls/login.php
+++ htmls/login.php
@@ -17,6 +17,7 @@ try{
     $stmt=$conn->prepare("SELECT * FROM users WHERE username='$username' AND password='$password'");
     $stmt->execute();
 	$userdb= $stmt->fetch();
+	global $conexion = "predo";
     if(count($userdb)>1)
     {
     	header('Location: http://logdecuentas.azurewebsites.net/htmls/paymentslogs.php');
