a4a241717c6ecedbde591f69525d789f849a98f7
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-08-04 14:32:16 -0400

    update loginIndex.php and session.php

--- htmls/session.php
+++ htmls/session.php
@@ -3,6 +3,36 @@ $server = "tcp:cszcc1h0ac.database.windows.net,1433";
 $user = "kindergame";
 $pwd = "baconPancakes#12345";
 $db = "lodDeCuentas_db";
+//loginIndex.php connection to db
+
+    if($_SERVER["REQUEST_METHOD"]=="POST")
+        {
+            if (isset($_POST['action'])) 
+            {
+                switch($_POST['action']) 
+                {
+                    case 'login':
+                    {
+                        if(empty($_POST['username'])||empty($_POST['password']))
+                        {
+                            $error="Username or password is invalid";
+                            echo "$error";
+                        }
+                        else
+                        {
+                            $username=$_POST['username'];
+                            $password=$_POST['password'];
+                            connectTodb();
+                        }
+                        break;
+                    }
+                    case 'otheraction':
+                        dosomethingelse();
+                        break;
+                }
+        }
+
+
 
 function connectTodb()
 {
@@ -22,7 +52,6 @@ try{
         $_SESSION['login_user']=$userdb['username'];
         header('Location: http://logdecuentas.azurewebsites.net/htmls/paymentslogs.php');
         
-    
     }
     else
     {
@@ -35,24 +64,97 @@ catch(Exception $e)
     {
     die(print_r($e));
     }
+}
 
+function signupForm()
+{
+$firstname=$lastname=$secondlastname=$username=$password=$email="";
+     $firstnameErr=$lastnameErr=$secondlastnameErr=$usernameErr=$passwordErr=$emailErr="";
+    $firstnameBool=$lasnameBool=$secondlastnameBool=$usernameBool=$passwordBool=$emailBool=FALSE;
 
+     if($_SERVER["REQUEST_METHOD"]=="POST")
+        {
+
+            if (empty($_POST["firstname"])) 
+            {
+                $firstnameErr = "Name is required";
+            } 
+            else 
+            {
+                if (!preg_match("/^[a-zA-Z ]*$/",$firstname)) 
+                {
+                $firstnameErr = "Only letters and white space allowed"; 
+                }
+                else
+                {
+                $firstname = test_input($_POST["firstname"]);
+                $firstnameBool=TRUE;
+                }
+            }
+
+            if (empty($_POST["email"])) 
+            {
+                $emailErr = "Email is required";
+            } 
+            else 
+            {
+                $email = test_input($_POST["email"]);
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+                {
+                $emailErr = "Invalid email format"; 
+                }
+                else
+                {
+                $emailBool=TRUE;
+                }
+                            
+            }
+            if (empty($_POST["lastname"])) 
+            {
+                $lastnameErr = "Lastname is required";
+            } 
+            else 
+            {
+                if (!preg_match("/^[a-zA-Z ]*$/",$lastname)) 
+                {
+                $lastnameErr = "Only letters and white space allowed"; 
+                }
+                else
+                {
+                $lastnameBool=TRUE;
+                $lastname= test_input($_POST["lastname"]);
+                }
+            
+            }
+            if (empty($_POST["password"])) 
+            {
+                $passwordErr = "Password is required";
+            } 
+            else 
+            {
+            $passwordBool=TRUE;
+            $password = test_input($_POST["password"]);
+            }
+            if (empty($_POST["username"])) 
+            {
+                $usernameErr = "username is Required";
+            } 
+            else 
+            {
+            $usernameBool=TRUE;
+            $username = test_input($_POST["username"]);
+            }
+
+    $secondlastnameBool=TRUE;
+    $secondlastname = test_input($_POST["secondLastname"]);
+     if($firstnameBool==TRUE and $lastnameBool==TRUE and $secondlastnameBool==TRUE and $emailBool==TRUE and $passwordBool==TRUE
+    and $usernameBool==TRUE)
+  
+    {
+    connectToDb($firstname,$lastname,$secondlastname,$email,$password,$username);
+    }
 
 }
-// catch(Exception $e)
-// 	{
-//     die(print_r($e));
-// 	}
 
-// session_start();// Starting Session
-// // Storing Session
-// $user_check=$_SESSION['login_user'];
-// // SQL Query To Fetch Complete Information Of User
-// $ses_sql=mysql_query("select username from login where username='$user_check'", $connection);
-// $row = mysql_fetch_assoc($ses_sql);
-// $login_session =$row['username'];
-// if(!isset($login_session)){
-// mysql_close($connection); // Closing Connection
-// header('Location: index.php'); // Redirecting To Home Page
 }
 ?>
