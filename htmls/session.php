b101a3147f18bdf1ea9cf4222aea08117f139219
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-08-10 14:30:04 -0400

    updating loginIndex and session.php and sigUp.php

--- htmls/session.php
+++ htmls/session.php
@@ -72,4 +72,101 @@ catch(Exception $e)
     }
 }
 
+funtion signUpControllerManager()
+ {
+    
+     $firstnameErr=$lastnameErr=$secondlastnameErr=$usernameErr=$passwordErr=$emailErr="";
+     $firstnameBool=$lasnameBool=$secondlastnameBool=$usernameBool=$passwordBool=$emailBool=FALSE;
+     $strPro = 'signUp';
+
+             if (empty($_POST["firstname"])) 
+             {
+                 $firstnameErr = "Name is required";
+             } 
+             else 
+             {
+                 if (!preg_match("/^[a-zA-Z ]*$/",$firstname)) 
+                 {
+                 $firstnameErr = "Only letters and white space allowed"; 
+                 }
+                 else
+                 {
+                 $firstname = test_input($_POST["firstname"]);
+                 $firstnameBool=TRUE;
+                 }
+             }
+
+             if (empty($_POST["email"])) 
+             {
+                 $emailErr = "Email is required";
+             } 
+             else 
+             {
+                 $email = test_input($_POST["email"]);
+                 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+                 {
+                 $emailErr = "Invalid email format"; 
+                 }
+                 else
+                 {
+                 $emailBool=TRUE;
+                 }
+                            
+             }
+             if (empty($_POST["lastname"])) 
+             {
+                 $lastnameErr = "Lastname is required";
+             } 
+             else 
+             {
+                 if (!preg_match("/^[a-zA-Z ]*$/",$lastname)) 
+                 {
+                 $lastnameErr = "Only letters and white space allowed"; 
+                 }
+                 else
+                 {
+                 $lastnameBool=TRUE;
+                 $lastname= test_input($_POST["lastname"]);
+                 }
+            
+             }
+             if (empty($_POST["password"])) 
+             {
+                 $passwordErr = "Password is required";
+             } 
+             else 
+             {
+             $passwordBool=TRUE;
+             $password = test_input($_POST["password"]);
+             }
+             if (empty($_POST["username"])) 
+             {
+                 $usernameErr = "username is Required";
+             } 
+             else 
+             {
+             $usernameBool=TRUE;
+             $username = test_input($_POST["username"]);
+             }
+
+     $secondlastnameBool=TRUE;
+     $secondlastname = test_input($_POST["secondLastname"]);
+      if($firstnameBool==TRUE and $lastnameBool==TRUE and $secondlastnameBool==TRUE and $emailBool==TRUE and $passwordBool==TRUE
+     and $usernameBool==TRUE)
+  
+     {
+     connectToDb($firstname,$password,$controller);
+     }
+   
+ }
+
+
+ function test_input($data) 
+ {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+ }
+
 ?>
