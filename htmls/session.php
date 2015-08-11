6d730e90003ead5595b70a1ef0d805e7d43a2f0c
Ricardo L. Vilches Sabalier <r.vilches26@gmail.com>
2015-08-11 12:25:41 -0400

    updating loginIndex and session.php and sigUp.php

--- htmls/session.php
+++ htmls/session.php
@@ -75,7 +75,7 @@ catch(Exception $e)
 function signUpControllerManager()
  {
 
-     $GLOBALS['firstnameErr']=$GLOBALS['lastnameErr']=$GLOBALS['secondlastnameErr']=$GLOBALS['$usernameErr']=$GLOBALS['$passwordErr']=$GLOBALS['emailErr']="";
+     $GLOBALS['firstnameErr']=$GLOBALS['lastnameErr']=$GLOBALS['secondlastnameErr']=$GLOBALS['$usernameErr']=$GLOBALS['$passwordErr']=$GLOBALS['emailErr']="3";
      $firstnameBool=$lasnameBool=$secondlastnameBool=$usernameBool=$passwordBool=$emailBool=FALSE;
      $controller = $_POST['controller'];
 
